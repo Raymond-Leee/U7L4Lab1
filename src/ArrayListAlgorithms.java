f2c60a3b887eb1158d7107bb9ba2c6de82262359
BT_1E10_12 <sample@bths.edu>
2022-02-08 12:18:21 -0500

    Commit 02/08/22

--- src/ArrayListAlgorithms.java
+++ src/ArrayListAlgorithms.java
@@ -184,17 +184,24 @@ public class ArrayListAlgorithms {
      *
      *  @param intList  intList of Integers
      */
-   // public static void removeDuplicates(ArrayList<Integer> intList)
+    public static void removeDuplicates(ArrayList<Integer> intList)
     {
-        // (int i = 0; i < intList.size(); i++)
+        for (int i = 0; i < intList.size(); i++)
         {
-
+            for (int j = 1 + i; j < intList.size(); j++)
+            {
+                if (intList.get(i) == intList.get(j))
+                {
+                    intList.remove(j);
+                    j--;
+                }
+            }
         }
     }
 
     /** Adds an uppercase version of each string directly AFTER the string
      *  in wordList; for example, if wordList is ["hello", "my", "best", "friend"]
-     *  this this method modifies wordList to be:
+     *  this method modifies wordList to be:
      *  ["hello", "HELLO", "my", "MY", "best", "BEST", "friend", "FRIEND"]
      *
      *  Assume all words have lowercase letters originally
@@ -232,6 +239,91 @@ public class ArrayListAlgorithms {
         for (int i = 0; i < length; i++)
         {
             wordList.add(wordList.get(i).toUpperCase());
+        }
+    }
+
+    /** Returns an arraylist of Strings that represents the input sentence parsed
+     *  into separate words (using a space: " " as the delimiter) and REVERSED
+     *  For example, if sentence = "This is my sentence!"
+     *  this method return [sentence!, my, is, This]
+     *
+     *  PRECONDITION: sentence does not end with a space
+     *
+     *  @param sentence  the input String that represents one or more words
+    separated by spaces
+     *  @return  new arraylist of Strings containing the words of sentence reversed
+     */
+    public static ArrayList<String> parseWordsAndReverse(String sentence)
+    {
+        ArrayList<String> temp = new ArrayList<String>();
+        ArrayList<String> reverse = new ArrayList<String>();
+        while (sentence.indexOf(" ") != -1)
+        {
+            temp.add(sentence.substring(0, sentence.indexOf(" ")));
+            sentence = sentence.substring(sentence.indexOf(" ") + 1);
+        }
+        temp.add(sentence);
+        for (int i = temp.size() - 1; i >= 0; i--)
+        {
+            reverse.add(temp.get(i));
+        }
+        return reverse;
+    }
+    /** Removes all words from wordList that begin with "b" and inserts them at the
+     *  front of wordList; all "b" words that are moved should appear in the same order
+     *  in the modified arrayList as they did before being moved
+     *
+     *  For example, this method will take a wordList:
+     *  ["apple", "banana", "cherry", "donut", "bagel", "danish", "berry", "baguette", "soda"]
+     *  and modify it to
+     *  ["banana", "bagel", "berry", "baguette", "apple", "cherry", "donut", "danish", "soda"]
+     *
+     *  DOES mutate (modify) elements in wordList
+     *  PRECONDITIONS: wordList.size() > 0, all strings in wordList have at least one character
+     *
+     *  @param wordList  arraylist of words
+     */
+    public static void moveBWords(ArrayList<String> wordList)
+    {
+        int insert = 0;
+        for (int i = 0; i < wordList.size(); i++)
+        {
+            if (wordList.get(i).substring(0, 1).equals("b"))
+            {
+                wordList.add(insert, wordList.get(i));
+                wordList.remove(i + 1);
+                insert++;
+            }
+        }
+    }
+    /** Returns Arraylist of Integers that contain all mode(s) of the int array numList.
+     *  If elements in numList all appear exactly once, there is no mode, and this method
+     *  should return an empty list
+     *
+     *  For example, if numList is: [1, 2, 3, 2, 4, 5, 5, 6],
+     *  then numList contains two modes: 2, 5
+     *  and this method returns an arraylist containing 2 and 5 (in any order)
+     *  If numList is: [1, 2, 3, 2, 4, 5, 5, 6, 6, 7, 6],
+     *  then numList contains one mode: 6
+     *  and this method returns an arrayList containing 6
+     *  If numList is: [1, 2, 3, 4, 5, 6], then numList contains no mode
+     *  and this method returns an empty arrayList: []
+     *  If numList is: [2, 2, 2, 3, 3, 3, 4, 4, 4],
+     *  then numList contains three modes: 2, 3, 4
+     *  and this method returns an arrayList containing 2, 3, and 4 (in any order)
+     *
+     *  Does NOT mutate (modify) elements in numList
+     *  PRECONDITIONS: numList.length > 0
+     *
+     *  @param numList  numList of ints
+     */
+    public static ArrayList<Integer> modes(int[] numList)
+    {
+        int currentOccurence = numList[0];
+        int maxOccurrence = 0;
+        for (int i = 0; i < numList.length; i++)
+        {
+
         }
     }
 }
